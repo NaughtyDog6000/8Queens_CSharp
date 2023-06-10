9107a696c1dfdfc4f9418a596e4cbabb951b91d2
nd6k <robbie.monty@hotmail.com>
2023-06-10 20:13:32 +0100

    first commit

---
+++ Queens_8/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Reflection.Emit;
+using System.Security.AccessControl;
+using System.Xml;
+
+namespace Queens_8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Board board = new Board();
+            
+        }
+
+
+        public struct Coordinate
+        {
+            public int XPos { get; set; }
+            public int YPos { get; set; }
+            
+            public Coordinate(int x, int y)
+            {
+                this.XPos = x;
+                this.YPos = y;
+            }
+
+            public static bool operator ==(Coordinate c1, Coordinate c2)
+            {
+                return c1.Equals(c2);
+            }
+            
+            public static bool operator !=(Coordinate c1, Coordinate c2)
+            {
+                return !c1.Equals(c2);
+            }
+
+            private bool Equals(Coordinate coord2)
+            {
+                if (this.XPos == coord2.XPos && this.YPos == coord2.YPos) return true;
+                return false;
+            }
+            
+        }
+
+        
+
+        public interface IPiece
+        {
+            void UpdatePosition(Coordinate coords);
+            List<Coordinate> GetTargetablePositions();
+            Coordinate GetCurrentPosition();
+
+        }
+
+        public class Board
+        {
+            //properties
+            private List<IPiece> Pieces = new List<IPiece>();
+
+            //Constructor
+
+            public Board()
+            {
+                
+            }
+            
+            
+            
+            //Methods
+            
+            public bool AddPieceToBoard(IPiece piece)
+            {
+                foreach (IPiece currentPiece in Pieces)
+                {
+                    if (piece.GetCurrentPosition() == currentPiece.GetCurrentPosition())
+                    {
+                        return false;
+                    }
+                    
+                }
+                
+                
+                this.Pieces.Add(piece);
+                return true;
+            }
+
+            public IPiece CheckPositionContents()
+            {
+                //should check the contents of a passed tile, returning the piece if filled
+                throw new NotImplementedException();
+                return null;
+            }
+
+        }
+    }
+}
---
+++ Queens_8/Queen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+
+namespace Queens_8
+{
+    public class Queen : Program.IPiece
+    {
+        //properties
+        
+        private Program.Coordinate _coords;
+
+
+        //Constructor
+
+        public Queen(Program.Coordinate coords)
+        {
+            this._coords = coords;
+        }
+        
+        
+        //methods
+        
+        public void UpdatePosition(Program.Coordinate coords) { this._coords = coords; }
+        public Program.Coordinate GetCurrentPosition() { return this._coords; }
+        
+
+        public List<Program.Coordinate> GetTargetablePositions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
